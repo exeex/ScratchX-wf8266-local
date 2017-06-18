029f3c09ef0c7216ae516efcca369cd36cce51a5
cswu <xray0h@gmail.com>
2017-06-18 20:48:39 +0800

    code reconstruct

--- StartScratchServer.py
+++ StartScratchServer.py
@@ -1,37 +1,41 @@
 #!/usr/bin/env python
- 
+
 import http.server
 import webbrowser
 import socket
 import urllib.request
-import queue
 
-Handler = http.server.SimpleHTTPRequestHandler
+
+def get_ip():
+    try:
+        ip = socket.gethostbyname(socket.gethostname())
+    except:
+        ip = "127.0.0.1"
+    return ip
+
 
 def run_server(server_class=http.server.HTTPServer, handler_class=http.server.SimpleHTTPRequestHandler):
     server_address = ('', 8000)
     httpd = server_class(server_address, handler_class)
     print("running scratch...")
-    try:
-        ip = socket.gethostbyname(socket.gethostname())
-    except:
-        ip = "127.0.0.1"
-    webbrowser.open("http://"+ip+":8000", new=0, autoraise=True)
+    ip = get_ip()
+    webbrowser.open("http://" + ip + ":8000", new=0, autoraise=True)
     httpd.serve_forever()
 
-try:
-    ip = socket.gethostbyname(socket.gethostname())
-except:
-    ip = "127.0.0.1"
 
-url = "http://{}:8000".format(ip)
+if __name__ == "__main__":
 
-try:
-    ret = urllib.request.urlopen(url)
-except:
-    ret = 0
+    # 取得本機ip
+    ip = get_ip()
+    url = "http://{}:8000".format(ip)
 
-if ret != 0 & ret.status == 200:
+    # 嘗試連線，若無法連線則開啟網頁伺服器
+    try:
+        ret = urllib.request.urlopen(url)
+
+    except (urllib.error.URLError, ConnectionRefusedError):
+        Handler = http.server.SimpleHTTPRequestHandler
+        run_server(server_class=http.server.HTTPServer, handler_class=Handler)
+
+    # 開啟瀏覽器
     webbrowser.open(url, new=0, autoraise=True)
-else:
-    run_server(server_class = http.server.HTTPServer, handler_class = Handler)
